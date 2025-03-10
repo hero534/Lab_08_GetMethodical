58fb82fd2e30270f0c339c5e9fefde3f89ae197a
hero534 <goldsmhr@mail.uc.edu>
2025-03-10 00:19:58 -0400

    Task 02 Complete

--- src/FavNumbers.java
+++ src/FavNumbers.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class FavNumbers {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num1 = 0;
+        double num2 = 0.00;
+        num1 = SafeInput.getInt(in, "Enter a integer: ");
+        num2 = SafeInput.getDouble(in, "Enter a double: ");
+        System.out.println("Your integer is " + num1 + " and your double is " + num2);
+
+    }
 }
